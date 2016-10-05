bb329780a7b1b7f73fc8724d69e9ee181f08ddf4
ser910 <degtiar.sergey@gmail.com>
2016-10-05 21:36:36 +0300

    work with blob

---
+++ AzureBlob/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureBlob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.
+                Parse(CloudConfigurationManager.
+                GetSetting("StorageConnectionString"));
+            //create the table client
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("newContaner");
+            container.CreateIfNotExists();
+            Console.WriteLine("create blob container");
+            container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            CloudBlockBlob blob = container.GetBlockBlobReference("pic1");
+            using (var fileStream = System.IO.File.OpenRead(@"path\myfile"))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+            //https://azure.microsoft.com/ru-ru/documentation/articles/storage-dotnet-how-to-use-blobs/
+        }
+    }
+}
