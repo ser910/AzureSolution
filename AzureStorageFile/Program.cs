489ecda2edd030ce0967af26a537f05ecdd047ce
ser910 <degtiar.sergey@gmail.com>
2016-10-06 20:59:38 +0300

    try tricks with azure

--- AzureStorageFile/Program.cs
+++ AzureStorageFile/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.File;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.File.Protocol;
 
 namespace AzureStorageFile
 {
@@ -34,9 +35,11 @@ namespace AzureStorageFile
                 CloudFile newFile = rootDir.GetFileReference("new.txt");
                 //newFile.UploadFromStream еще не допилено
                 newFile.UploadText("пример файла в root директории");
+
                 CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                 CloudBlobContainer container = blobClient.GetContainerReference("filecontainer");
                 container.CreateIfNotExists();
+
                 CloudBlockBlob destBlob = container.GetBlockBlobReference("fileToBlob.txt");
                 string fileSas = newFile.GetSharedAccessSignature(new SharedAccessFilePolicy()
                 {
@@ -44,7 +47,29 @@ namespace AzureStorageFile
                     SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24) //доступ к файлу на 24 часа
                 });
                 Uri fileSasUri = new Uri(newFile.StorageUri.PrimaryUri.ToString() + fileSas);
-                destBlob.StartCopy(fileSasUri); 
+                destBlob.StartCopy(fileSasUri);
+                ShareStats stats = share.GetStats();
+                Console.WriteLine("Current share usage:{0}",stats.Usage.ToString());
+                Console.WriteLine("Current share quota:{0}",share.Properties.Quota.ToString());
+                share.Properties.Quota += 1;
+                share.FetchAttributes();
+                Console.WriteLine("Current share quota:{0}", share.Properties.Quota.ToString());
+
+                string polisyName = "sampleSharePolicy" + DateTime.UtcNow.Ticks; //KEY
+                SharedAccessFilePolicy sharedPolicy = new SharedAccessFilePolicy()//KEY BODY
+                {
+                    SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+                    Permissions = SharedAccessFilePermissions.Read | SharedAccessFilePermissions.Write
+                };
+                FileSharePermissions permissions = share.GetPermissions();
+                permissions.SharedAccessPolicies.Add(polisyName, sharedPolicy);
+
+
+                string sasToken = newFile.GetSharedAccessSignature(null, polisyName);
+                fileSasUri = new Uri(newFile.StorageUri.PrimaryUri.ToString() + sasToken);
+                var newfileSas = new CloudFile(fileSasUri);
+                newfileSas.UploadText("this auth with...");
+                //DownloadFileFromShare(newfileSas.DownloadText());
             }
             Console.WriteLine("Program end");
             Console.ReadKey(true);
